5dd703a6f54dd7a2defa0d3ee65467588fd7df08
ryozilla <kanosuki_12553322@outlook.com>
2022-07-24 21:43:59 +0800

    给train.t9.Complex8做了一个递归函数（GCD()）计算最大公因数（因为看了b站的崩服报告）

--- src/train/t9/Complex8.java
+++ src/train/t9/Complex8.java
@@ -11,11 +11,56 @@ import methods.Maths;   //计算最大公约数方法所在处
 public class Complex8 {
     public static void main(String[] args) {
         try {
-            System.out.println(Maths.calcGCD(3, 5));
-            System.out.println(Maths.calcGCD(4, 8));
-            System.out.println(Maths.calcGCD(9, -6));
+            System.out.println(calcGCD(3, 5));
+            System.out.println(calcGCD(4, 8));
+            System.out.println(calcGCD(9, -6));
         } catch (NumberErrorException e) {
             e.printStackTrace();
         }
+    }
+
+    private static int calcGCD(int x, int y) throws NumberErrorException {
+        //计算两个数的最大公因数
+        //判断数字是否为负整数
+        if (x < 0) {
+            throw new NumberErrorException("输入的数不应该为负整数: x = " + x);
+        } else if (y < 0) {
+            throw new NumberErrorException("输入的数不应该为负整数: y = " + y);
+        }
+
+        //主方法
+        if (x == y) {
+            //两数相等
+            return x;
+        } else if (Maths.isPrime(x) && Maths.isPrime(y)) {
+            //两数同为质数
+            return 1;
+        } else {
+            /*
+             * 辗转相除法
+             * a ÷ b = num …… c
+             * b -> a , c -> b;
+             * 循环至c = 0为止
+             * */
+            //从大到小排序
+            int a, b;
+            if (x > y) {
+                a = x;
+                b = y;
+            } else {
+                a = y;
+                b = x;
+            }
+            return GCD(a, b);
+        }
+    }
+
+    private static int GCD(int a, int b) {
+        int c = a % b;
+        if (c == 0) {
+            return b;
+        }
+
+        return GCD(b,c);
     }
 }
